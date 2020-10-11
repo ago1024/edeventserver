6683db7112b972fa58bde060d7ff937db167490c
Alexander Gottwald <alexander.gottwald@gmail.com>
2020-10-11 15:32:30 +0200

    [frontend] pi-navigation-list test

--- eddisplay/src/app/poi-navigation/poi-navigation-list/poi-navigation-list.component.spec.ts
+++ eddisplay/src/app/poi-navigation/poi-navigation-list/poi-navigation-list.component.spec.ts
@@ -1,10 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PoiNavigationService } from '../poi-navigation.service';
 
 import { PoiNavigationListComponent } from './poi-navigation-list.component';
 
 describe('PoiNavigationListComponent', () => {
+	let service: PoiNavigationService;
 	let component: PoiNavigationListComponent;
 	let fixture: ComponentFixture<PoiNavigationListComponent>;
+
+	const poi = {
+		Name: 'Crashed drone',
+		Latitude: -9.933245,
+		Longitude: -50.169037,
+		BodyName: 'Luchtaine A 2 a',
+	};
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
@@ -14,6 +23,7 @@ describe('PoiNavigationListComponent', () => {
 	});
 
 	beforeEach(() => {
+		service = TestBed.inject(PoiNavigationService);
 		fixture = TestBed.createComponent(PoiNavigationListComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
@@ -22,4 +32,18 @@ describe('PoiNavigationListComponent', () => {
 	it('should create', () => {
 		expect(component).toBeTruthy();
 	});
+
+	it('should show pois', () => {
+		service.pois = [{...poi}, {...poi}];
+		fixture.detectChanges();
+		expect(fixture.nativeElement.querySelectorAll('li').length).toBe(2);
+	});
+
+	it('should remove a poi', () => {
+		service.pois = [{...poi}, {...poi}];
+		fixture.detectChanges();
+		fixture.nativeElement.querySelector('button').click();
+		expect(service.pois.length).toBe(1);
+	});
+
 });
