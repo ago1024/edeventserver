6e5c909b39192fb8ef1f88982ab027f0a0a88b8c
Alexander Gottwald <alexander.gottwald@gmail.com>
2025-04-06 16:14:08 +0200

    update eslint

--- eddisplay/eslint.config.mjs
+++ eddisplay/eslint.config.mjs
@@ -1,69 +1,53 @@
-import { defineConfig } from "eslint/config";
+// @ts-check
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import angular from "angular-eslint";
 import stylistic from "@stylistic/eslint-plugin";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default defineConfig([{
+export default tseslint.config(
+  {
     plugins: {
-        "@stylistic": stylistic,
+      "@stylistic": stylistic,
     },
-}, {
+  },
+  {
     files: ["**/*.ts"],
-    extends: compat.extends("plugin:@angular-eslint/recommended"),
-
-    languageOptions: {
-        ecmaVersion: 5,
-        sourceType: "script",
-
-        parserOptions: {
-            project: ["tsconfig.*?.json"],
-            createDefaultProgram: true,
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@stylistic/indent": ["warn", "tab", {
+        SwitchCase: 1,
+      }],
+      "@stylistic/no-trailing-spaces": "error",
+      "@angular-eslint/directive-selector": [
+        "error",
+        {
+          type: "attribute",
+          prefix: "app",
+          style: "camelCase",
         },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        {
+          type: "element",
+          prefix: "app",
+          style: "kebab-case",
+        },
+      ],
     },
-
-    rules: {
-        "@stylistic/indent": ["warn", "tab", {
-            SwitchCase: 1,
-        }],
-
-        "@stylistic/no-trailing-spaces": "error",
-
-        "@angular-eslint/directive-selector": ["error", {
-            type: "attribute",
-            prefix: "app",
-            style: "camelCase",
-        }],
-
-        "@angular-eslint/component-selector": ["error", {
-            type: "element",
-            prefix: "app",
-            style: "kebab-case",
-        }],
-
-        "@stylistic/quotes": ["error", "single", {
-            allowTemplateLiterals: true,
-        }],
-    },
-}, {
-    files: ["**/*.component.html"],
-    extends: compat.extends("plugin:@angular-eslint/template/recommended"),
-
-    rules: {
-        "max-len": ["error", {
-            code: 200,
-        }],
-    },
-}, {
-    files: ["**/*.component.ts"],
-    extends: compat.extends("plugin:@angular-eslint/template/process-inline-templates"),
-}]);
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
