66290e9243f27210f1e600870200781e5c1a62ea
Alexander Gottwald <alexander.gottwald@gmail.com>
2025-04-07 00:44:49 +0200

    Move motherlode text-to-speech to its own method

--- eddisplay/src/app/mining-event-list/mining-event-list.component.ts
+++ eddisplay/src/app/mining-event-list/mining-event-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { MiningEventComponent } from '../mining-event/mining-event.component';
-import { MiningEventListService } from './mining-event-list.service';
+import { MiningEventListService, ProspectedAsteroidEvent } from './mining-event-list.service';
 import { AsyncPipe, PercentPipe } from '@angular/common';
-import { filter, tap } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -23,18 +23,21 @@ export class MiningEventListComponent {
 
 	private readonly subscription = this.service.prospected$.pipe(
 		takeUntilDestroyed(),
-		filter(() => this.soundEnabled()),
-		filter(() => !!speechSynthesis),
-		filter((event) => !!event.MotherlodeMaterial),
-		tap(event => {
-			try {
-				const utterance = new SpeechSynthesisUtterance(event.MotherlodeMaterial);
-				utterance.lang = 'en';
-				speechSynthesis.speak(utterance);
-			} catch (error) {
-				console.error(error);
-			}
-		}),
-	).subscribe();
+		filter((event) => event.MotherlodeMaterial && this.soundEnabled()),
+	).subscribe(event => this.announceMotherlode(event));
+
+	protected announceMotherlode(event: ProspectedAsteroidEvent): void {
+		if (!speechSynthesis) {
+			return;
+		}
+
+		try {
+			const utterance = new SpeechSynthesisUtterance(event.MotherlodeMaterial);
+			utterance.lang = 'en';
+			speechSynthesis.speak(utterance);
+		} catch (error) {
+			console.error(error);
+		}
+	}
 
 }
